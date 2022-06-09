1b9fb4e4eade610affdb87a06ae626250345a3b4
Niki Hite <nikihite@comcast.net>
2022-06-08 17:34:17 -0700

    styled chakra page

--- src/Chakras/ChakrasList.js
+++ src/Chakras/ChakrasList.js
@@ -1,8 +1,9 @@
 import ChakraItem from './ChakraItem';
+import './chakras.css';
 
 export default function ChakrasList({ chakras }) {
   return (
-    <div>
+    <div className='chakra2'>
       {
         chakras.map((chakra, i) => <ChakraItem {...chakra} key={i}/>)
       }
